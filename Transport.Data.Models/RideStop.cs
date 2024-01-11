8ab19f71fec68719200dc60e56600f5cae9eec30
AEManov20 <AEManov20@codingburgas.bg>
2024-01-11 22:32:58 +0200

    Add scaffolded models

---
+++ Transport.Data.Models/RideStop.cs
@@ -0,0 +1,14 @@
+﻿namespace Transport.Data.Models;
+
+public partial class RideStop
+{
+    public Guid Id { get; set; }
+
+    public Guid RideId { get; set; }
+
+    public string AddressText { get; set; } = null!;
+
+    public short OrderingNumber { get; set; }
+
+    public virtual Ride Ride { get; set; } = null!;
+}
