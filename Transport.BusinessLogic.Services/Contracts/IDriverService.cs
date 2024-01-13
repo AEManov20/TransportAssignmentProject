85f6fcf55e7f4e2f9e9d3ab2292be2972464848a
AEManov20 <AEManov20@codingburgas.bg>
2024-01-13 15:39:22 +0200

    Lay out driver service interface

---
+++ Transport.BusinessLogic.Services/Contracts/IDriverService.cs
@@ -0,0 +1,24 @@
+﻿using Transport.BusinessLogic.Models;
+using Transport.Data.Models;
+
+namespace Transport.BusinessLogic.Services.Contracts;
+
+public enum DriverAvailability : short
+{
+    Available,
+    Driving,
+    Offline
+}
+
+public interface IDriverService
+{
+    Task<Tuple<Driver?, string?>> CreateDriverAsync(DriverInputModel driver);
+
+    Task<Driver?> GetDriverById(Guid id);
+
+    Task<Driver?> GetDriverByVehicleIdAsync(Guid vehicleId);
+
+    Task<Tuple<Driver?, string?>> UpdateDriverVehicleAsync(Guid vehicleId);
+
+    Task<Tuple<bool, string?>> DeleteDriverById(Guid id);
+}
