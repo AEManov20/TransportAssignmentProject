1738237140d35c7e5631912ee53a722498467a4f
AEManov20 <AEManov20@codingburgas.bg>
2024-01-13 15:39:41 +0200

    Lay out ride service interface

---
+++ Transport.BusinessLogic.Services/Contracts/IRideService.cs
@@ -0,0 +1,32 @@
+﻿using Transport.BusinessLogic.Models;
+using Transport.Data.Models;
+
+namespace Transport.BusinessLogic.Services.Contracts;
+
+public enum RideStatus : short
+{
+    Requested,
+    Accepted,
+    TookOff,
+    Finalized,
+    Cancelled
+}
+
+public interface IRideService
+{
+    Task<Tuple<Ride?, string?>> CreateRideAsync(RideInputModel ride, Guid riderId);
+
+    Task<Ride?> CancelRideAsync(Guid id);
+
+    Task<Ride?> MarkRideAcceptedAsync(Guid id);
+
+    Task<Ride?> MarkRideTookOffAsync(Guid id);
+
+    Task<Ride?> MarkRideArrivedAsync(Guid id);
+
+    Task<Tuple<Ride?, string?>> UpdateUserReviewId(Guid id, Guid userReviewId);
+
+    Task<ICollection<Ride>?> GetDriverRidesAsync(Guid driverId, bool isOnGoing = false);
+
+    Task<ICollection<Ride>?> GetUserRidesAsync(Guid userId, bool isOnGoing = false);
+}
