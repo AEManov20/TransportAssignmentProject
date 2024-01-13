9e99fd1db59b64d94993f64b8f0150a9e71ca948
AEManov20 <AEManov20@codingburgas.bg>
2024-01-13 15:39:51 +0200

    Lay out user service interface

---
+++ Transport.BusinessLogic.Services/Contracts/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using Transport.BusinessLogic.Models;
+using Transport.Data.Models;
+
+namespace Transport.BusinessLogic.Services.Contracts;
+
+public interface IUserService
+{
+    Task<Tuple<User?, string?>> CreateUserAsync(UserInputModel user);
+    
+    Task<User?> GetUserByIdAsync(Guid id);
+
+    Task<User?> GetUserByEmailAsync(string email);
+
+    Task<Tuple<User?, string?>> UpdateUserAsync(UserInputModel user);
+
+    Task<Tuple<bool, string?>> DeleteUserAsync(Guid id);
+
+    Task<Tuple<bool, string?>> UpdateUserPassword(Guid id, string oldPassword, string newPassword);
+}
