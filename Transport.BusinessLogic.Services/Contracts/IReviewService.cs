c3410d072582e404c97e17e585a7ddf397ed7801
AEManov20 <AEManov20@codingburgas.bg>
2024-01-13 15:39:31 +0200

    Lay out review service interface

---
+++ Transport.BusinessLogic.Services/Contracts/IReviewService.cs
@@ -0,0 +1,12 @@
+﻿using Transport.BusinessLogic.Models;
+
+namespace Transport.BusinessLogic.Services.Contracts;
+
+public interface IReviewService
+{
+    Task<Tuple<UserReviewViewModel?, string?>> CreateReview(UserReviewInputModel userReview);
+
+    Task<Tuple<bool, string?>> DeleteReviewById(Guid id);
+
+    Task<UserReviewViewModel?> GetReviewById(Guid id);
+}
