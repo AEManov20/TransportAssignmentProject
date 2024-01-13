7eacb42155cd8c401d90594052f4bba7d6a97b4e
AEManov20 <AEManov20@codingburgas.bg>
2024-01-13 15:38:31 +0200

    Add view models

---
+++ Transport.BusinessLogic.Models/UserReviewViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Transport.BusinessLogic.Models;
+
+public class UserReviewViewModel
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public double Rating { get; set; }
+
+    public string Content { get; set; } = string.Empty;
+
+    public UserViewModel Author { get; set; }
+}
