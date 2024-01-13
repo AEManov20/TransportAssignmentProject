0c4b27242d6664fa704f242e033ab2a9ba919256
AEManov20 <AEManov20@codingburgas.bg>
2024-01-13 15:40:02 +0200

    Lay out vehicle service interface

---
+++ Transport.BusinessLogic.Services/Contracts/IVehicleService.cs
@@ -0,0 +1,15 @@
+﻿using Transport.BusinessLogic.Models;
+using Transport.Data.Models;
+
+namespace Transport.BusinessLogic.Services.Contracts;
+
+public interface IVehicleService
+{
+    Task<Vehicle?> GetVehicleById(Guid id);
+
+    Task<Tuple<Vehicle?, string?>> CreateVehicle(VehicleInputModel vehicle);
+
+    Task<Tuple<Vehicle?, string?>> UpdateVehicle(VehicleInputModel vehicle);
+
+    Task<Tuple<bool, string?>> DeleteVehicle(Guid id);
+}
