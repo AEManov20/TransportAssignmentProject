d12757c523849846a6d4f0c3790d60b8c59522fb
AEManov20 <AEManov20@codingburgas.bg>
2024-01-15 00:29:49 +0200

    Update server configuration

--- Transport.WebHost/Program.cs
+++ Transport.WebHost/Program.cs
@@ -1,5 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using Transport.Data;
+using Microsoft.AspNetCore.Identity;
+using Transport.BusinessLogic.Services;
+using Transport.Data.Models;
+using Transport.WebHost;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +34,17 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.EnableDetailedErrors();
 });
 
+builder.Services
+    .AddIdentity<User, IdentityRole<Guid>>(opt =>
+    {
+        opt.User.RequireUniqueEmail = true;
+        opt.SignIn.RequireConfirmedAccount = false;
+        opt.SignIn.RequireConfirmedEmail = false;
+        opt.SignIn.RequireConfirmedPhoneNumber = false;
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
 if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddCors(options =>
@@ -38,8 +58,41 @@ if (builder.Environment.IsDevelopment())
     });
 }
 
-var app = builder.Build();
+builder.Services.AddLogging(config =>
+{
+    config.AddConsole();
+});
 
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ClockSkew = TimeSpan.Zero,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtAccessTokenSecret"]!)),
+        };
+    });
+
+builder.Services.AddControllers(options =>
+    options.ModelMetadataDetailsProviders.Add(new SystemTextJsonValidationMetadataProvider()));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAutoMapper(typeof(Mappings));
+builder.Services.AddServices();
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -47,6 +100,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 app.UseHttpsRedirection();
 app.UseCors();
