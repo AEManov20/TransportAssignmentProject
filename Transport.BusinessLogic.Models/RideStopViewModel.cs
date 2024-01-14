137b7b639cfc929ad45e7fc4ca6535620da79f91
AEManov20 <AEManov20@codingburgas.bg>
2024-01-14 22:39:47 +0200

    Add ride stop view model

---
+++ Transport.BusinessLogic.Models/RideStopViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Transport.Data.Models;
+
+namespace Transport.BusinessLogic.Models;
+
+public class RideStopViewModel
+{
+    public string AddressText { get; set; } = null!;
+
+    public short OrderingNumber { get; set; }
+}
