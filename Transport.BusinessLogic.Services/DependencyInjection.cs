85937628c70e73a7cd26f7d510b6e6f1c57930dd
AEManov20 <AEManov20@codingburgas.bg>
2024-01-14 22:44:04 +0200

    Add extension method for services

---
+++ Transport.BusinessLogic.Services/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Transport.BusinessLogic.Services.Contracts;
+using Transport.BusinessLogic.Services.Implementations;
+
+namespace Transport.BusinessLogic.Services;
+
+public static class DependencyInjection
+{
+    public static void AddServices(this IServiceCollection servicesCollection)
+    {
+        servicesCollection
+            .AddScoped<IDriverService, DriverService>()
+            .AddScoped<IUserService, UserService>()
+            .AddScoped<IAuthService, AuthService>()
+            .AddScoped<IReviewService, ReviewService>()
+            .AddScoped<IRideService, RideService>()
+            .AddScoped<ITokenService, TokenService>();
+    }
+}
