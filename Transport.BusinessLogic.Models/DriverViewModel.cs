3d9727c93c11d673ed65ada49315c308026ed35b
AEManov20 <AEManov20@codingburgas.bg>
2024-01-14 22:39:38 +0200

    Add driver view model

---
+++ Transport.BusinessLogic.Models/DriverViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Transport.Data.Models;
+
+namespace Transport.BusinessLogic.Models;
+
+public class DriverViewModel
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public DriverAvailability AvailabilityStatus { get; set; }
+
+    public VehicleViewModel Vehicle { get; set; } = null!;
+}
