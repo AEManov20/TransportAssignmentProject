6451c9345280ee0d9389da48be793e741fda0d84
AEManov20 <AEManov20@codingburgas.bg>
2024-01-14 22:40:16 +0200

    Add vehicle view model

---
+++ Transport.BusinessLogic.Models/VehicleViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Transport.BusinessLogic.Models;
+
+public class VehicleViewModel
+{
+    public string Brand { get; set; } = string.Empty;
+
+    public string Model { get; set; } = string.Empty;
+
+    public string RegistrationNumber { get; set; } = string.Empty;
+
+    public string RegisteredInCountry { get; set; } = string.Empty;
+
+    public short Seats { get; set; }
+
+    public string Color { get; set; } = string.Empty;
+}
