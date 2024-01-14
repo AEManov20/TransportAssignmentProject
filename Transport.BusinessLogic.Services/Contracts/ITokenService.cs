580361617c68284cd279e5a3d310a0de76f7c9fe
AEManov20 <AEManov20@codingburgas.bg>
2024-01-14 22:41:31 +0200

    Lay out token service

---
+++ Transport.BusinessLogic.Services/Contracts/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿
+using System.IdentityModel.Tokens.Jwt;
+
+public interface ITokenService
+{
+    /// <summary>
+    /// Creates an access token for a user
+    /// </summary>
+    /// <param name="userId">User ID</param>
+    /// <returns>The new access token</returns>
+    Task<JwtSecurityToken?> CreateTokenForUserAsync(Guid userId);
+}
