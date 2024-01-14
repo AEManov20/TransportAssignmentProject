eed753492e6dd2b0fcee50c0047055fd5841f3b7
AEManov20 <AEManov20@codingburgas.bg>
2024-01-14 22:41:23 +0200

    Lay out authentication service

---
+++ Transport.BusinessLogic.Services/Contracts/IAuthService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Transport.Data.Models;
+using Transport.BusinessLogic.Models;
+
+namespace Transport.BusinessLogic.Services.Contracts;
+
+public interface IAuthService
+{
+    /// <summary>
+    /// Checks if user exists via supplied email
+    /// </summary>
+    /// <param name="email">Email of the user</param>
+    /// <returns>A boolean which tells whether the user exists</returns>
+    Task<bool> CheckUserExistsAsync(string email);
+
+    /// <summary>
+    /// Checks if user's provided password is correct.
+    /// </summary>
+    /// <param name="email">Email of the user</param>
+    /// <param name="password">Password of the user</param>
+    /// <returns>A boolean which tells whether the password is correct</returns>
+    Task<bool> CheckPassword(string email, string password);
+
+    /// <summary>
+    /// Tells whether a user can sign in or not provided he has the correct password
+    /// </summary>
+    /// <param name="email">Email of the user</param>
+    /// <returns>A boolean which tells whether a user can sign in or not</returns>
+    Task<bool> CanUserSignIn(string email);
+}
