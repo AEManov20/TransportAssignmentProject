3f474899ee988177e0ddebf2d550b6807e478d08
AEManov20 <AEManov20@codingburgas.bg>
2024-01-14 23:14:19 +0200

    Add model mappings

---
+++ Transport.WebHost/Mappings.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Transport.BusinessLogic.Models;
+using Transport.Data.Models;
+
+namespace Transport.WebHost;
+
+public class Mappings : Profile
+{
+    public Mappings()
+    {
+        CreateMap<UserInputModel, User>();
+        CreateMap<UserReviewInputModel, UserReview>();
+        CreateMap<VehicleInputModel, Vehicle>();
+
+        CreateMap<User, UserViewModel>();
+        CreateMap<Vehicle, VehicleViewModel>();
+        CreateMap<Driver, DriverViewModel>();
+        CreateMap<UserReview, UserReviewViewModel>();
+        CreateMap<RideStop, RideStopViewModel>();
+        CreateMap<Ride, RideViewModel>();
+    }
+}
