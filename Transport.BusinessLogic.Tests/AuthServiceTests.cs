7d1a63adef4744ebb7fc222d8e1a883c2ec9e7d8
aemanov20 <aemanov20@codingburgas.bg>
2024-01-15 14:35:57 +0200

    Add auth service tests

--- Transport.BusinessLogic.Tests/AuthServiceTests.cs
+++ Transport.BusinessLogic.Tests/AuthServiceTests.cs
@@ -1,46 +1,102 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using NUnit.Framework;
 
 [TestFixture]
 public class AuthServiceTests
 {
     private IAuthService authService;
+    private ApplicationDbContext context;
+    private UserManager<User> userManager;
+    private SignInManager<User> signInManager;
+    private IMapper mapper;
+
+    private IServiceCollection services;
 
     [SetUp]
-    public void Setup()
+    public async Task Setup()
     {
-        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-            .UseInMemoryDatabase(databaseName: "Transport")
-            .Options;
+        services = new ServiceCollection();
 
+        services.AddAutoMapper(typeof(Mappings));
 
-        authService = new AuthService();
+        services.AddDbContext<ApplicationDbContext>(opt =>
+        {
+            opt.UseInMemoryDatabase(databaseName: "TransportDatabase");
+            opt.EnableSensitiveDataLogging();
+            opt.EnableThreadSafetyChecks();
+            opt.EnableDetailedErrors();
+        });
+
+        services.AddIdentity<User, IdentityRole<Guid>>(opt =>
+            {
+                opt.User.RequireUniqueEmail = true;
+                opt.SignIn.RequireConfirmedAccount = false;
+                opt.SignIn.RequireConfirmedEmail = false;
+                opt.SignIn.RequireConfirmedPhoneNumber = false;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddLogging();
+
+        var provider = services.BuildServiceProvider();
+
+        context = provider.GetRequiredService<ApplicationDbContext>();
+        mapper = provider.GetRequiredService<IMapper>();
+        userManager = provider.GetRequiredService<UserManager<User>>();
+        signInManager = provider.GetRequiredService<SignInManager<User>>();
+
+        authService = new AuthService(userManager, signInManager);
+
+        // Seed example data
+        await userManager.CreateAsync(
+                    mapper.Map<User>(new UserInputModel()
+                    {
+                        Email = "testing1@example.com",
+                        FirstName = "John",
+                        LastName = "Doe",
+                        PhoneNumber = "+359888888888",
+                        UserName = "john_doe"
+                    }), "12341234");
+
+        await userManager.CreateAsync(
+                    mapper.Map<User>(new UserInputModel()
+                    {
+                        Email = "testing2@example.com",
+                        FirstName = "Jane",
+                        LastName = "Doe",
+                        PhoneNumber = "+359888888889",
+                        UserName = "jane_doe"
+                    }), "12341234");
+
     }
 
     [Test]
-    public void Test_Login_Success()
+    public async Task Test_CheckUserExistsAsync_Valid()
     {
         // Arrange
-        string username = "testuser";
-        string password = "testpassword";
+        string email = "testing1@example.com";
 
         // Act
-        bool result = authService.Login(username, password);
+        var result = await authService.CheckUserExistsAsync(email);
 
         // Assert
-        Assert.IsTrue(result);
+        Assert.That(result, Is.True);
     }
 
     [Test]
-    public void Test_Login_Failure()
+    public async Task Test_CheckUserExistsAsync_Invalid()
     {
         // Arrange
-        string username = "testuser";
-        string password = "wrongpassword";
+        string email = "testing@example.com";
 
         // Act
-        bool result = authService.Login(username, password);
+        var result = await authService.CheckUserExistsAsync(email);
 
         // Assert
-        Assert.IsFalse(result);
+        Assert.That(result, Is.False);
     }
 }
--- Transport.BusinessLogic.Tests/GlobalUsings.cs
+++ Transport.BusinessLogic.Tests/GlobalUsings.cs
@@ -1,3 +1,12 @@
 global using NUnit.Framework;
+
 global using Transport.BusinessLogic.Models;
-global using Transport.BusinessLogic.Services;
+
+global using Transport.BusinessLogic.Services.Contracts;
+global using Transport.BusinessLogic.Services.Implementations;
+
+global using Transport.Data.Models;
+global using Transport.Data;
+global using Transport.WebHost;
+
+global using Microsoft.EntityFrameworkCore;
